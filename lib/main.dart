336bec587cbe0a9b0697a346741ae239ef7e885a
ElitonRoberto <elitongt45@gmail.com>
2023-05-05 19:41:40 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatefulWidget {
+  _MeuApp() createState() => _MeuApp();
+}
+
+class _MeuApp extends State<MeuApp>{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      
+    );
+  }
+}
